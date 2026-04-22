0aa9a44a3c9d3161badc01751bdd4f5ff842686f
ISIOMA123123 <ubanisuccess26@gmail.com>
2026-04-22 14:40:05 +0100

    Update main.jsx

--- invoice-app/src/main.jsx
+++ invoice-app/src/main.jsx
@@ -1,13 +1,17 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { InvoiceProvider } from "./context/InvoiceContext";
 import "./styles/main.css";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <InvoiceProvider>
+      <App />
+    </InvoiceProvider>
   </BrowserRouter>
 );
 
