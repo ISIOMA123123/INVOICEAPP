6f2bca08ccb2388340143a5ea60c774887633e88
ISIOMA123123 <ubanisuccess26@gmail.com>
2026-04-22 10:58:17 +0100

    Update InvoiceList.jsx

--- invoice-app/src/pages/InvoiceList.jsx
+++ invoice-app/src/pages/InvoiceList.jsx
@@ -2,20 +2,15 @@ import invoices from "../data/invoices";
 import InvoiceCard from "../components/InvoiceCard";
 
 function InvoiceList() {
-    return (
-        <div>
-            <h2>Invoices</h2>
+  return (
+    <div>
+      <h2>Invoices</h2>
 
-            {invoices.map((invoice) => (
-                <div key={invoice.id}>
-                    <h3>#{invoice.id}</h3>
-                    <p>{invoice.clientName}</p>
-                    <p>£ {invoice.amount}</p>
-                    <p>{invoice.status}</p>
-                </div>
-            ))}
-        </div>
-    );
+      {invoices.map((invoice) => (
+        <InvoiceCard key={invoice.id} invoice={invoice} />
+      ))}
+    </div>
+  );
 }
 
 export default InvoiceList;
