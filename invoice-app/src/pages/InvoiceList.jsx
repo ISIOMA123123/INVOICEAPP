f2b9002a4f0a6fc8b3bef01d9679ba265b6c8def
ISIOMA123123 <ubanisuccess26@gmail.com>
2026-04-22 15:23:42 +0100

    update app.jsx

--- invoice-app/src/pages/InvoiceList.jsx
+++ invoice-app/src/pages/InvoiceList.jsx
@@ -4,13 +4,19 @@ import { useContext } from "react";
 import { InvoiceContext } from "../context/InvoiceContext";
 import InvoiceCard from "../components/InvoiceCard";
 
+import { useNavigate } from "react-router-dom";
 
 function InvoiceList() {
+  const navigate = useNavigate();
   const { invoices } = useContext(InvoiceContext);
 
   return (
     <div>
       <h2>Invoices</h2>
+
+      <button onClick={() => navigate("/new")}>
+        + New Invoice
+      </button>
 
       {invoices.length === 0 ? (
         <p>No invoices yet</p>
@@ -22,6 +28,7 @@ function InvoiceList() {
     </div>
   );
 }
+
 
 
 export default InvoiceList;
