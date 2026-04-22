d0870bd97296a3bfd748c5ab7d5f3563387fc299
ISIOMA123123 <ubanisuccess26@gmail.com>
2026-04-22 15:20:37 +0100

    Update App.jsx

--- invoice-app/src/App.jsx
+++ invoice-app/src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
 import { Routes, Route } from "react-router-dom";
 import InvoiceList from "./pages/InvoiceList";
 import InvoiceDetail from "./pages/InvoiceDetail";
+import NewInvoice from "./pages/NewInvoice";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<InvoiceList />} />
       <Route path="/invoice/:id" element={<InvoiceDetail />} />
+      <Route path="/new" element={<NewInvoice />} />
     </Routes>
   );
 }
 
 export default App;
+
